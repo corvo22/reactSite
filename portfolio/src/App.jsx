b129b3653ae300d5c13544c4e954072d53c8cd92
Ky McCormick <kdm2013red@gmail.com>
2025-03-18 17:13:54 -0400

    React setup

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react';
+import ReactMarkdown from 'react-markdown'
+import './App.css'
+
+function SectionContent({section_id}) {
+  const [markdownContent, setMarkdownContent] = useState('');
+
+  // defaults to a GET
+  useEffect(() => {
+    fetch(`/api/section/${section_id}`)
+      .then(response => response.text())
+      .then(markdown => setMarkdownContent(markdown))
+      .catch(error => console.error(error))
+  }, [section_id]);
+
+  return (
+    <div className='display-screen'>
+      <ReactMarkdown>{markdownContent}</ReactMarkdown>
+    </div>
+  )
+}
+
+function App() {
+  const [currentSection, setCurrentSection] = useState(null);
+
+  return (
+    <div className="layout-container">
+      <div className="left-col">
+        <div className="icon">icon</div>
+        <div className="spotify">spotify</div>
+      </div>
+      <div className="right-col">
+        <nav>
+          <button className="button" onClick={() => setCurrentSection("about")}>About</button>
+          <button className="button" onClick={() => console.log('Go to about')}>Resume</button>
+          <button className="button" onClick={() => console.log('Go to contact')}>Projects</button>
+          <button className="button" onClick={() => console.log('Go to contact')}>Writing</button>
+          <button className="button" onClick={() => console.log('Go to contact')}>Git</button>
+        </nav>
+        <div className="content-area">
+          {currentSection ? (
+            <SectionContent sectionId={currentSection} />
+          ) : (
+            <div className="display-screen"></div>
+          )}
+          <div className="today-info">info</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App
---
+++ portfolio/src/sections/about.md
@@ -0,0 +1,9 @@
+*   Name: Ky McCormick
+*   Pronouns: They/Them
+*   Hobbies: Bass, Indie Games, DnD, Poker, Fantasy/Sci-fi
+*   Current Book: Les Mis
+
+- - -
+
+Hello, I am a Computer Science student at Drexel University  
+This website serves as a hub for my projects. These include a representation of my resume. I am very interested in the intersection of arts/music and computer science systems programming, data structures/algorithms, and computer science education. I endeavor to become a professor or teacher after getting my bachelors
