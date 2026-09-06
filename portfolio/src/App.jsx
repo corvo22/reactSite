aa78d1cc847b4a48c9aa60c0c45453b97a5ce2a6
kymc <kdm2013red@gmail.com>
2026-09-06 06:00:03 +0000

    Update daily message

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -115,62 +115,62 @@ function App() {
             <hr></hr>
             <h3>Pre-1600</h3>
             <ul>
-              <li>394 – On the first day of the battle of Frigidus, the Western Roman troops of Arbogast manage to defend their positions against the Eastern Roman troops of emperor Theodosius I.</li>
-              <li>1590 – An army led by Alexander Farnese, Duke of Parma forces Henry IV of France to lift the siege of Paris.</li>
-              <li>1234 – The Decretals of Gregory IX, a new canonical collection assembled by Raymond of Peñafort, are promulgated.</li>
+              <li>394 – Battle of the Frigidus: Roman emperor Theodosius I defeats and kills Eugenius the usurper. His Frankish magister militum Arbogast escapes but commits suicide two days later.</li>
+              <li>1492 – Christopher Columbus sails from La Gomera in the Canary Islands, his final port of call before crossing the Atlantic Ocean for the first time.</li>
+              <li>1522 – The Victoria returns to Sanlúcar de Barrameda in Spain, the only surviving ship of Ferdinand Magellan's expedition and the first known ship to circumnavigate the world.</li>
             </ul>
             <h3>Early Modern</h3>
             <ul>
-              <li>1816 – Louis XVIII of France has to dissolve the Chambre introuvable ("Unobtainable Chamber").</li>
-              <li>1798 – Conscription is made mandatory in France by the Jourdan law.</li>
-              <li>1725 – Wedding of Louis XV and Maria Leszczyńska.</li>
+              <li>1863 – American Civil War: Confederate forces evacuate Battery Wagner and Morris Island in South Carolina.</li>
+              <li>1634 – Thirty Years' War: In the Battle of Nördlingen, the Catholic Imperial army defeats Swedish and German Protestant forces.</li>
+              <li>1870 – Louisa Ann Swain of Laramie, Wyoming becomes the first woman in the United States to cast a vote legally after 1807.</li>
             </ul>
             <h3>Modern</h3>
             <ul>
-              <li>1991 – The current international treaty defending indigenous peoples, Indigenous and Tribal Peoples Convention, 1989, comes into force.</li>
-              <li>1945 – Cold War: Igor Gouzenko, a Soviet Union embassy clerk, defects to Canada, exposing Soviet espionage in North America, signalling the beginning of the Cold War.</li>
-              <li>1915 – The pacifist Zimmerwald Conference begins.</li>
+              <li>2018   – Brazilian presidential candidate Jair Bolsonaro survives a stabbing at a campaign rally in Juiz de Fora, Minas Gerais.</li>
+              <li>2018 – Supreme Court of India decriminalises all consensual sex among adults in private, making homosexuality legal on the Indian lands.</li>
+              <li>1943   – Pennsylvania Railroad's premier train derails at Frankford Junction in Philadelphia, killing 79 people and injuring 117 others.</li>
             </ul>
             <hr></hr>
             <h2>Births:</h2>
             <hr></hr>
             <h3>Pre-1600</h3>
             <ul>
-              <li>1451 – Isabel Neville, daughter of Richard Neville (died 1476)</li>
-              <li>1540 – Magnus of Holstein, prince of Denmark  (died 1583)</li>
-              <li>1201 – Alix of Thouars, duchess of Brittany (died 1221)</li>
+              <li>1475   – Sebastiano Serlio, Italian Mannerist architect (died 1554)</li>
+              <li>1475 – Artus Gouffier, Lord of Boissy, French nobleman and politician (died 1519)</li>
+              <li>1802 – Alcide d'Orbigny, French zoologist, palaeontologist, and geologist (died 1857)</li>
             </ul>
             <h3>Early Modern</h3>
             <ul>
-              <li>1876 – Wilhelm Ritter von Leeb, German field marshal (died 1956)</li>
-              <li>1667 – Giovanni Girolamo Saccheri, Italian priest, mathematician, and philosopher (died 1733)</li>
-              <li>1642 – Maria of Orange-Nassau, Dutch princess (died 1688)</li>
+              <li>1892 – Edward Victor Appleton, English-Scottish physicist and academic, Nobel Prize laureate (died 1965)</li>
+              <li>1861 – William Lane, English-Australian journalist, founded New Australia (died 1917)</li>
+              <li>1954   – Demetris Kizas, Cypriot footballer</li>
             </ul>
             <h3>Modern</h3>
             <ul>
-              <li>1978 – Chris Hipkins, New Zealand politician, 41st Prime Minister of New Zealand</li>
-              <li>1965 – David Brabham, Australian race car driver</li>
-              <li>1994 – Kings Elliot, Swiss-English singer</li>
+              <li>1984   – William Porterfield, Northern Irish cricketer</li>
+              <li>1967 – William DuVall, American singer-songwriter and guitarist</li>
+              <li>394 – Eugenius, Roman usurper</li>
             </ul>
             <hr></hr>
             <h2>Deaths:</h2>
             <hr></hr>
             <h3>Pre-1600</h3>
             <ul>
-              <li>590 – Authari, Lombard king (born 540)</li>
-              <li>714 – Shang, emperor of the Tang Dynasty</li>
-              <li>1235 – Henry I, duke of Brabant (born 1165)</li>
+              <li>1276 – Vicedomino de Vicedominis, Italian cardinal (born 1210)</li>
+              <li>1178 – Ioveta, Latin princess</li>
+              <li>1885 – Narcís Monturiol, Spanish engineer, designed the Ictineo I and Ictineo II (born 1819)</li>
             </ul>
             <h3>Early Modern</h3>
             <ul>
-              <li>1876 – Manuel Blanco Encalada, Chilean admiral and politician, 1st President of Chile (born 1790)</li>
-              <li>1894 – George Stoneman, Jr., United States Army cavalry officer (born 1822)</li>
-              <li>1803 – François Devienne, French flute player and composer (born 1759)</li>
+              <li>1868 – Pierre Adolphe Rost, American lawyer, judge, and politician (born 1797)</li>
+              <li>1708 – Sir John Morden, 1st Baronet, English merchant and philanthropist, founded Morden College (born 1623)</li>
+              <li>1952 – Gertrude Lawrence, English actress, singer, and dancer (born 1898)</li>
             </ul>
             <h3>Modern</h3>
             <ul>
-              <li>1999   – Allen Funt, American director, producer, and screenwriter (born 1914)</li>
-              <li>2016 – Hugh O'Brian, American actor (born 1925)</li>
+              <li>2012 – Elisabeth Böhm, German architect (born 1921)</li>
+              <li>2017   – Kate Millett, American feminist author and activist (born 1934)</li>
               <li>2024   – Laurent Tirard, French film director and screenwriter (born 1967)</li>
             </ul>
           </div>
