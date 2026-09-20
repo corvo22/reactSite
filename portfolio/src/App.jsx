ddf2ad26c0ad7005b04d766e4bed31eea749b8f9
kymc <kdm2013red@gmail.com>
2026-09-20 06:00:03 +0000

    Update daily message

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -115,63 +115,63 @@ function App() {
             <hr></hr>
             <h3>Pre-1600</h3>
             <ul>
-              <li>634 – Siege of Damascus: The Rashidun Arabs under Khalid ibn al-Walid capture Damascus from the Byzantine Empire.</li>
-              <li>1356 – Battle of Poitiers: An English army under the command of Edward the Black Prince defeats a French army and captures King John II.</li>
-              <li>1410 – End of the Siege of Marienburg: The State of the Teutonic Order repulses the joint Polish—Lithuanian forces.</li>
+              <li>1519 – Ferdinand Magellan sets sail from Sanlúcar de Barrameda with about 270 men on his expedition which ultimately culminates in the first circumnavigation of the globe.</li>
+              <li>1066 – At the Battle of Fulford, Harald Hardrada defeats earls Morcar and Edwin.</li>
+              <li>1586 – A number of conspirators in the Babington Plot are hanged, drawn and quartered.</li>
             </ul>
             <h3>Early Modern</h3>
             <ul>
-              <li>1893 – In New Zealand, the Electoral Act of 1893 is consented to by the governor, giving all women in New Zealand the right to vote.</li>
-              <li>1870 – Franco-Prussian War: The siege of Paris begins. The city held out for over four months before surrendering.</li>
-              <li>1863 – American Civil War: The first day of the Battle of Chickamauga, in northwestern Georgia, the bloodiest two-day battle of the conflict, and the only significant Confederate victory in the war's Western Theater.</li>
+              <li>1870 – The Bersaglieri corps enter Rome through the Porta Pia, and complete the unification of Italy.</li>
+              <li>1860 – The future King Edward VII of the United Kingdom begins the first visit to North America by a Prince of Wales.</li>
+              <li>1863 – American Civil War: The Battle of Chickamauga, in northwestern Georgia, ends in a Confederate victory.</li>
             </ul>
             <h3>Modern</h3>
             <ul>
-              <li>1950 – Korean War: An attack by North Korean forces was repelled at the Battle of Nam River.</li>
-              <li>1970 – Michael Eavis hosts the first Glastonbury Festival.</li>
-              <li>1976   – Two Imperial Iranian Air Force F-4 Phantom II jets fly out to investigate an unidentified flying object.</li>
+              <li>2000 – The United Kingdom's MI6 Secret Intelligence Service building is attacked by individuals using a Russian-built RPG-22 anti-tank missile.</li>
+              <li>1954 – The Moomin comics, created by Tove Jansson and Lars Jansson, is published internationally in the London newspaper The Evening News.</li>
+              <li>2011 – The United States military ends its "don't ask, don't tell" policy, allowing gay men and women to serve openly for the first time.</li>
             </ul>
             <hr></hr>
             <h2>Births:</h2>
             <hr></hr>
             <h3>Pre-1600</h3>
             <ul>
-              <li>1377 – Albert IV, Duke of Austria (died 1404)[citation needed]</li>
-              <li>931 – Mu Zong, emperor of the Liao Dynasty (died 969)</li>
-              <li>1477 – Ferrante d'Este, Ferrarese nobleman and condottiero (died 1540)</li>
+              <li>1504 – Philip III, Count of Nassau-Weilburg (died 1559)</li>
+              <li>1449 – Philipp I, Count of Hanau-Münzenberg (died 1500)</li>
+              <li>1593 – Gottfried Scheidt, German organist and composer (died 1661)</li>
             </ul>
             <h3>Early Modern</h3>
             <ul>
-              <li>1867 – Arthur Rackham, English illustrator (died 1939)</li>
-              <li>1882 – Christopher Stone, English radio host (died 1965)</li>
-              <li>1898 – Giuseppe Saragat, Italian lawyer and politician, 5th President of Italy (died 1988)</li>
+              <li>1895 – Walter Dubislav, German logician and philosopher of science (died 1937)</li>
+              <li>1819 – Frederick Ellsworth Sickels, American inventor (died 1895)</li>
+              <li>1851 – Henry Arthur Jones, English playwright and critic (died 1929)</li>
             </ul>
             <h3>Modern</h3>
             <ul>
-              <li>1949   – Barry Scheck, American lawyer, co-founded the Innocence Project</li>
-              <li>1936 – Martin Fay, Irish fiddler (died 2012)</li>
-              <li>1996   – Chris Silva, Gabonese basketball player</li>
+              <li>1928 – Alberto de Lacerda, Mozambican-Portuguese poet and radio host (died 2007)</li>
+              <li>1973   – Jo Pavey, English runner</li>
+              <li>1983   – Ángel Sánchez, Puerto Rican baseball player</li>
             </ul>
             <hr></hr>
             <h2>Deaths:</h2>
             <hr></hr>
             <h3>Pre-1600</h3>
             <ul>
-              <li>1589 – Jean-Antoine de Baïf, French poet (born 1532)</li>
-              <li>643 – Goeric of Metz, Frankish bishop and saint</li>
-              <li>1339 – Emperor Go-Daigo of Japan (born 1288)</li>
+              <li>1533 – Veit Stoss, German sculptor (born c. 1447)</li>
+              <li>1440 – Frederick I, Elector of Brandenburg (born 1371)</li>
+              <li>1460 – Gilles Binchois, Flemish composer (born 1400)</li>
             </ul>
             <h3>Early Modern</h3>
             <ul>
-              <li>1843 – Gaspard-Gustave de Coriolis, French mathematician,  physicist, and engineer (born 1792)</li>
-              <li>1692 – Giles Corey, American farmer and accused wizard (born c. 1612)</li>
-              <li>1881 – James A. Garfield, American general, lawyer, and politician, and the 20th President of the United States (born 1831)</li>
+              <li>1815 – Nicolas Desmarest, French geologist and scholar (born 1725)</li>
+              <li>1639 – Johannes Meursius, Dutch historian and scholar (born 1579)</li>
+              <li>1839 – Sir Thomas Hardy, 1st Baronet, English admiral (born 1769)</li>
             </ul>
             <h3>Modern</h3>
             <ul>
-              <li>2017 – Leonid Kharitonov, Russian bass-baritone (born 1933)</li>
-              <li>2009 – Milton Meltzer, American historian and author (born 1915)</li>
-              <li>1975 – Pamela Brown, English actress (born 1917)</li>
+              <li>1971 – Giorgos Seferis, Greek poet and diplomat, Nobel Prize laureate (born 1900)</li>
+              <li>1979 – Ludvík Svoboda, Czech general and politician, 8th President of Czechoslovakia (born 1895)</li>
+              <li>1999 – Robert Lebel, Canadian businessman (born 1905)</li>
             </ul>
           </div>
         </div>
